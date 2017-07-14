cbb96dd8dd641a027431faf60e4ce4c291a9a5c6
bryn2008 <brynla.lloyd@gmail.com>
2017-07-14 11:51:35 +0100

    added comments for directions

--- ChapterOne.java
+++ ChapterOne.java
@@ -1,7 +1,16 @@
 public class ChapterOne {
 
 	public static void main(String[] args){
+
+		//Try running javac ChapterOne.java
+		//Then run java ChapterOne "Shepton Mallet" Frome
+
 		System.out.println(args[0]);
 		System.out.println(args[1]);
+
+		//You should see the following:
+		//Shepton Mallet
+		//Frome
+
 	}
 }
