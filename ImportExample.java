f188620b839be9e49884b2e45f36d12b7bcdcc53
bryn2008 <brynla.lloyd@gmail.com>
2017-07-14 11:59:20 +0100

    using import statment

--- ImportExample.java
+++ ImportExample.java
@@ -1,6 +1,8 @@
+import java.util.Random; // tells java to import the Random package and where to find it
+
 public class ImportExample {
 	public static void main(String[] args){
 		Random r = new Random();
-		System.out.println(r.nextInt(10));
+		System.out.println(r.nextInt(10)); //prints a number between 0 and 9
 	}
 }
