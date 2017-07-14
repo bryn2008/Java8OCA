e1bc8edc8bdbd4d099b61821f4982cbadc5e6d94
bryn2008 <brynla.lloyd@gmail.com>
2017-07-14 15:34:38 +0100

    java primitive types

---
+++ PrimativeDataTypes.java
@@ -0,0 +1,15 @@
+public class PrimativeDataTypes {
+
+	public static void main(String[] args){
+
+		System.out.println("boolean \t" + (0 == 0)); // Boolean true or false
+		System.out.println("byte \t\t" + Byte.MAX_VALUE);
+		System.out.println("short \t\t" + Short.MAX_VALUE);
+		System.out.println("int \t\t" + Integer.MAX_VALUE);
+		System.out.println("long \t\t" + Long.MAX_VALUE);
+		System.out.println("float \t\t" + Float.MAX_VALUE);
+		System.out.println("double \t\t" + Double.MAX_VALUE);
+		System.out.println("char \t\t" + "a"); // Char
+		
+	}
+}
